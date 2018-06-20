8b94ca3233e3fbfb2e92d7f1a36b7a726a749de5
Mathias <mathias.sellam@essec.edu>
2018-06-20 07:35:03 +0200

    Mathias2006

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.use('/image', express.static(__dirname + '/image'));
 app.get('/', function(req, res) {
   res.sendFile('home.html',{ root: __dirname });
 });
+app.use(function findPokemon(){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+      
+    var dbo = db.db("pokemon");
+    dbo.collection("pokedex").findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result.name);
+        db.close();
+      });
+});
+})
 app.get('/recrutement', function(req,res){
     res.sendFile('recrutement.html',{root:__dirname});
 });
@@ -23,4 +40,6 @@ app.get('/news', function(req,res){
 app.get('/stylesheet.css', function(req, res) {
     res.sendFile(__dirname + "/" + "stylesheet.css");
   });
+
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
